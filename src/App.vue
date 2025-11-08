99dd0df18e48eacff0c543fc7c264341deb74107
Sergey Chertov <sergey@chertov.ru>
2025-11-08 18:03:38 +0700

    include MainPage to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
-<script setup></script>
+<script setup>
+import MainPage from './views/MainPage.vue'
+
+</script>
 
 <template>
   <h1>You did it!</h1>
@@ -6,6 +9,7 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <MainPage />
 </template>
 
 <style scoped></style>
